a65ee4e12e891bfae0256ee93d3cd84d5aadd165
Jane Sternbach <janesternbach@gmail.com>
2013-03-16 13:53:40 -0400

    initial commit: hopefully not db.yml file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chyldswelfare::Application.config.secret_token = 'b28af90df244a23c2448d8dea5b1208c07e628fdc6bb8c0fe3fbb0107e6f4c19d830d94f6893a93b7587f808a9d2f0afb59a50989d1cc38935bbeab553b554d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Chyldswelfare::Application.config.session_store :cookie_store, key: '_chyldswelfare_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Chyldswelfare::Application.config.session_store :active_record_store
